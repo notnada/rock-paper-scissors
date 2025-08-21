0d585980f31dd8183cebfc97d6eb65bea9a1fd31
notnada <121617189+notnada@users.noreply.github.com>
2025-08-22 00:53:25 +0100

    Add playRound() function definition and choice constants
    function is called to play one round of rock paper scissors, also add constants to get each the computer's and human's choice and make it lower case to avoid any conflict in comparing it

--- rps.js
+++ rps.js
@@ -18,7 +18,26 @@ function getHumanChoice() {
 	return prompt("Enter your choice");
 }
 
+// function for playing one round
+function playRound(humanChoice, computerChoice) {
+	let combo = `${humanChoice},${computerChoice}`;
+	if ( combo == "rock,scissors"|| combo == "scissors,paper" || combo == "paper,rock") {
+		humanScore++;
+		return `Human wins! ${humanChoice} beats ${computerChoice}`;
+	
+	} else if ( combo == "scissors,rock"|| combo == "paper,scissors" || combo == "rock,paper") {
+		computerScore++;
+		return `Computer wins! ${computerChoice} beats ${humanChoice}`;
+
+	} else {
+		return "it's a tie!"
+	}
+}
+
 // variables to store player's and computer's scores
 let humanScore = 0;
 let computerScore = 0;
+// constants to store player's and computer's choice
+const computerChoice = getComputerChoice();
+const humanChoice = getHumanChoice().toLowerCase();
 
