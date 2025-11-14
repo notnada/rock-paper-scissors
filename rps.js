9e61f4266fc5b8997944163c12b9e9a47c4151b1
notnada <121617189+notnada@users.noreply.github.com>
2025-11-14 09:05:20 +0100

    Add simple ui to game and change logic to conform

--- rps.js
+++ rps.js
@@ -1,70 +1,85 @@
 
 // function that returns the computer's choice in the game
 function getComputerChoice() {
+	const choices = ["rock", "paper", "scissors"]
 	// Gives a random number 0,1 or 2
 	let computerChoice = Math.floor(Math.random()* 3 ); 
 	// Converts it into a choice : rock, paper or scissors
-	if (computerChoice == 0) {
-		return "rock";
-	} else if (computerChoice == 1) {
-		return "paper";
-	} else {
-		return "scissors"
-	}
+	return choices[computerChoice];
 }
+	const box = document.getElementById("message");
 
-// function that returns the human's choice in the game
-function getHumanChoice() {
-	return prompt("Enter your choice");
-}
-
-// function to play a game that consists of 5 rounds
-function playGame () {
-	
-	// variables to store player's and computer's scores
-	let humanScore = 0;
-	let computerScore = 0;
-
-	// declaring the playround parameters
-	let humanChoice = "";
-	let computerChoice ="";
-
-	// function for playing one round
 	function playRound(humanChoice, computerChoice) {
+		// variables to store player's and computer's scores
+		let humanScoreInt = parseInt(humanScore.textContent);
+		let computerScoreInt = parseInt(computerScore.textContent);
+		
+
 		// store player's and computer's choice
 		computerChoice= getComputerChoice();
-		humanChoice = getHumanChoice().toLowerCase();
+		
 		const combo = `${humanChoice},${computerChoice}`;
 		if ( combo == "rock,scissors"|| combo == "scissors,paper" || combo == "paper,rock") {
-			humanScore++;
-			console.log(`Human wins! ${humanChoice} beats ${computerChoice}`);
+			humanScoreInt++;
+			humanScore.textContent = humanScoreInt
+			box.textContent = `Human wins! ${humanChoice} beats ${computerChoice}`;
+			
 
 			} else if ( combo == "scissors,rock"|| combo == "paper,scissors" || combo == "rock,paper") {
-				computerScore++;
-				console.log(`Computer wins! ${computerChoice} beats ${humanChoice}`);
+				computerScoreInt++;
+				computerScore.textContent = computerScoreInt
+				box.textContent = `Computer wins! ${computerChoice} beats ${humanChoice}`;
 		} else {
-			console.log("it's a tie!");
+			
+
+			box.textContent = "it's a tie!";   // show text
+
 				}
+	
 	}
 
-	// Call playround 5 times
-	playRound ( humanChoice, computerChoice);
-	playRound ( humanChoice, computerChoice);
-	playRound ( humanChoice, computerChoice);
-	playRound ( humanChoice, computerChoice);
-	playRound ( humanChoice, computerChoice);
 
-	// Show Scores
-	console.log(`Human Score ${humanScore}`)
-	console.log(`Computer Score ${computerScore}`)
+function getHumanChoice(onChoice) {
+  let buttons = document.querySelectorAll("button");
 
-	// Decide the winner
-	if ( humanScore > computerScore ) {
-		console.log("Human wins the game!");
-	} else {
-		console.log("Computer wins the game!");
-	}
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      onChoice(button.innerText);
+    });
+  });
 
 }
+// declaring the playround parameters
+	let humanChoice = "";
+	let computerChoice ="";
+// Call it and define what happens with the choice
 
-playGame();
+
+
+
+	let humanScore = document.querySelector("#humanscore")
+	let computerScore = document.querySelector("#computerscore")
+
+	getHumanChoice((choice) => {
+   		humanChoice = choice;
+   		playRound(humanChoice,computerChoice)
+
+   		
+if (humanScore.textContent== 5) {
+
+ 	box.textContent = "human wins!"
+ 	humanScore.innerText= 0;
+ 	computerScore.innerText = 0;
+} else if (computerScore.textContent == 5) {
+   box.textContent = "computer wins!"
+  humanScore.innerText= 0;
+ 	computerScore.innerText = 0;
+}
+	
+			});
+
+	
+
+
+
+
